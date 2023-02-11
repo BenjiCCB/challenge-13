962a5d1add1484dcad1bba5c519584fd50feb77c
BenjiCCB <bensdecker+CCB@gmail.com>
2023-02-11 16:01:33 -0500

    starting routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,33 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+// find all categories & their associated products
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+// find one category by its `id` value (and its associated products)
+router.get('/:id', async (req, res) => {
+  // try {
+  //   const locationData = await Location.findByPk(req.params.id, {
+  //     // JOIN with travellers, using the Trip through table
+  //     include: [{ model: Traveller, through: Trip, as: 'location_travellers' }]
+  //   });
+
+  //   if (!locationData) {
+  //     res.status(404).json({ message: 'No location found with this id!' });
+  //     return;
+  //   }
+
+  //   res.status(200).json(locationData);
+  // } catch (err) {
+  //   res.status(500).json(err);
+  // }
 });
 
 router.post('/', (req, res) => {
